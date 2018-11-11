22a2398c30045ce5260a3dbafb3cdff0dacc823c
Naveen Kumaravelmurugaian <oknaveen9395@gmail.com>
2018-11-11 23:46:04 +0530

    addition is done in add method and is displayed in view(jsp)

--- SpringMVCApplication/src/main/java/com/naveen/addController.java
+++ SpringMVCApplication/src/main/java/com/naveen/addController.java
@@ -3,8 +3,12 @@
  */
 package com.naveen;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author admin
@@ -13,7 +17,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class addController {
 	@RequestMapping("/add")
-	public void add() {
-		System.out.println("hi");
+	public ModelAndView add(HttpServletRequest request, HttpServletResponse response) {
+		int i = Integer.parseInt(request.getParameter("t1"));
+		int j = Integer.parseInt(request.getParameter("t2"));
+		int k = i + j;
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("display.jsp");
+		mv.addObject("result", k);
+		return mv;
 	}
 }
